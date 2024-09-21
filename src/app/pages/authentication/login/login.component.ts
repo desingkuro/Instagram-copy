9e1551525634eef7c2bef2577003f19a946e93b9
desingkuro <andresmauricio745@gmail.com>
2024-09-21 14:45:11 -0500

    ver y ocultar password

--- src/app/pages/authentication/login/login.component.ts
+++ src/app/pages/authentication/login/login.component.ts
@@ -8,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+    typeInput:boolean = false;
+    textBtnPassword:string = 'Mostrar'
+    user = {
+      email:'',
+      password:''
+    }
 
+    changeType(){
+      if(this.typeInput){
+        this.typeInput = false
+      }else{
+        this.typeInput = true
+      }
+    }
+
+    handleSubmit(event: Event): void {
+      event.preventDefault();
+      const { email, password } = this.getData(event);
+  
+      this.user.email = JSON.stringify(email);
+      this.user.password = JSON.stringify(password);
+  
+      this.validateForm();
+    }
+  
+    getData(event: Event) {
+      const query = new window.FormData(event.target as HTMLFormElement);
+      const email = query.get('email');
+      const password = query.get('password');
+      return { email, password };
+    }
+    
+    validateForm() {
+      if (this.user.email && this.user.password) {
+        //console.log(this.user.email);
+        //console.log(this.user.password);
+      } else {
+        console.error('Email o contraseña no pueden estar vacíos');
+      }
+    }
 }
